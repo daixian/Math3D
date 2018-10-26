f02f1d085080f3bef408ea0aad2b906be789320a
daixian <amano_tooko@qq.com>
2018-10-26 20:46:09 +0800

    构建一个Unity的仿真场景

---
+++ Simulation/Assets/Calibration/Calibration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calibration : MonoBehaviour {
+
+    /// <summary>
+    /// 棋盘格
+    /// </summary>
+    public Transform board;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
