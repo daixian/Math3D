2bb5ae023236b3526c853706ab4ff5fcc6592153
daixian <amano_tooko@qq.com>
2018-12-28 06:01:04 +0800

    增加一个vs code的c++小实验

---
+++ VSCodeCpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int, char **)
+{
+
+    for (size_t i = 0; i < 100; i++)
+    {
+        std::cout << i << std::endl;
+    }
+
+    std::cout << "Hello, world!\n";
+}
