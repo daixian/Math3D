58db26184a5d30f6d285b30e2f3ee8c48d95f4ce
daixian <amano_tooko@qq.com>
2019-02-16 13:13:40 +0800

    调整了模型资源的缩放

--- Simulation/Assets/Display/Estimate.cs
+++ Simulation/Assets/Display/Estimate.cs
@@ -10,29 +10,37 @@ using UnityEditor;
 /// <summary>
 /// 画辅助线
 /// </summary>
-public class Estimate : MonoBehaviour {
+public class Estimate : MonoBehaviour
+{
 
-    private void Awake() {
-
-    }
-
-    void Start() {
+    private void Awake()
+    {
 
     }
 
-    void Update() {
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
 
     }
 
 #if UNITY_EDITOR
-    void OnDrawGizmos() {
+    void OnDrawGizmos()
+    {
         //对着两个节点画连线
-        if (Selection.transforms.Length == 2) {
+        if (Selection.transforms.Length == 2)
+        {
             Transform tran1 = Selection.transforms[0];
             Transform tran2 = Selection.transforms[1];
-            if (tran1 != null && tran2 != null) {
+            if (tran1 != null && tran2 != null)
+            {
                 Handles.DrawDottedLine(tran1.position, tran2.position, 0.01f);
-                Handles.Label((tran1.position + tran2.position) / 2, $"{(tran1.position - tran2.position).magnitude:F3}", new GUIStyle() {
+                Handles.Label((tran1.position + tran2.position) / 2, $"{(tran1.position - tran2.position).magnitude:F3}", new GUIStyle()
+                {
                     fontSize = 9
                 });
             }
@@ -40,27 +48,35 @@ public class Estimate : MonoBehaviour {
         }
 
         //如果只选中了一个物体
-        if (Selection.transforms.Length == 1) {
+        if (Selection.transforms.Length == 1)
+        {
             Transform tranRoot = Selection.transforms[0];
 
             //对着一个大节点画所有的线
-            if (tranRoot.childCount >= 2) { //如果有子级
-                for (int i = 0; i < tranRoot.childCount - 1; i++) {
+            if (tranRoot.childCount >= 2)
+            { //如果有子级(子级个数大于1)
+                for (int i = 0; i < tranRoot.childCount - 1; i++)
+                {
                     Transform tran1 = tranRoot.GetChild(i);
                     Transform tran2 = tranRoot.GetChild(i + 1);
-                    if (tran1 != null && tran2 != null) {
+                    if (tran1 != null && tran2 != null)
+                    {
                         Vector3 p1 = tran1.position;
                         Vector3 p2 = tran2.position;
                         Handles.DrawLine(p1, p2);
-                        Handles.Label((p1 + p2) / 2, $"{(p1 - p2).magnitude:F3}", new GUIStyle() {
+                        Handles.Label((p1 + p2) / 2, $"{(p1 - p2).magnitude:F3}", new GUIStyle()
+                        {
                             fontSize = 9
                         });
                     }
                 }
 
-            } else { //如果没有子级
+            }
+            else
+            { //如果没有子级
                 Handles.DrawLine(Vector3.zero, tranRoot.position);
-                Handles.Label(tranRoot.position / 2, $"{tranRoot.position.magnitude:F3}", new GUIStyle() {
+                Handles.Label(tranRoot.position / 2, $"{tranRoot.position.magnitude:F3}", new GUIStyle()
+                {
                     fontSize = 9
                 });
             }
