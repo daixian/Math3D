58091b67f64e95e075111acb1dffc8f442ea5ec3
daixian <amano_tooko@qq.com>
2019-03-07 17:38:36 +0800

    当载入文件之后,设置当前文件路径到检视面板

--- Simulation/Assets/Display/Editor/ObserveEditor.cs
+++ Simulation/Assets/Display/Editor/ObserveEditor.cs
@@ -5,33 +5,42 @@ using System.Collections.Generic;
 using System.IO;
 using System;
 
-namespace dxlib {
+namespace dxlib
+{
 
-[CustomEditor(typeof(dxlib.Observe))]
-public class ObserveEditor : Editor {
+    [CustomEditor(typeof(dxlib.Observe))]
+    public class ObserveEditor : Editor
+    {
 
-    private void Awake() {
-    }
-
-    public override void OnInspectorGUI() {
-        DrawDefaultInspector();
-
-        dxlib.Observe myScript = (dxlib.Observe)target;
-        //在检视面板显示一个按钮
-        if (GUILayout.Button("重新载入json场景")) {
-            myScript.LoadFile(myScript.jsonPath);
+        private void Awake()
+        {
         }
 
-        //显示10条历史记录
-        string[] history = Config.Inst.GetHistory();
-        for (int i = 0; i < 10; i++) {
-            if (history != null && history.Length > i) {
-                FileInfo fi = new FileInfo(history[i]);
-                if (GUILayout.Button(fi.Name)) {
-                    myScript.LoadFile(fi.FullName);
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            dxlib.Observe myScript = (dxlib.Observe)target;
+            //在检视面板显示一个按钮
+            if (GUILayout.Button("重新载入json场景"))
+            {
+                myScript.LoadFile(myScript.jsonPath);
+            }
+
+            //显示10条历史记录
+            string[] history = Config.Inst.GetHistory();
+            for (int i = 0; i < 10; i++)
+            {
+                if (history != null && history.Length > i)
+                {
+                    FileInfo fi = new FileInfo(history[i]);
+                    if (GUILayout.Button(fi.Name))
+                    {
+                        myScript.LoadFile(fi.FullName);
+                        myScript.jsonPath = fi.FullName;
+                    }
                 }
             }
         }
     }
-}
 }
