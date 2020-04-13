6addbad58c6061027870a969e469c88a25a4b2ae
daixian <amano_tooko@qq.com>
2020-04-14 02:15:24 +0800

    对接了新的标定方法的实验场景

--- Simulation/Assets/Code/Simulation/Display/Estimate.cs
+++ Simulation/Assets/Code/Simulation/Display/Estimate.cs
@@ -39,7 +39,7 @@ public class Estimate : MonoBehaviour
             if (tran1 != null && tran2 != null)
             {
                 Handles.DrawDottedLine(tran1.position, tran2.position, 0.01f);
-                Handles.Label((tran1.position + tran2.position) / 2, $"{(tran1.position - tran2.position).magnitude:F3}", new GUIStyle()
+                Handles.Label((tran1.position + tran2.position) / 2, $"{(tran1.position - tran2.position).magnitude * 100.0f:F3}", new GUIStyle()
                 {
                     fontSize = 9
                 });
@@ -64,7 +64,7 @@ public class Estimate : MonoBehaviour
                         Vector3 p1 = tran1.position;
                         Vector3 p2 = tran2.position;
                         Handles.DrawLine(p1, p2);
-                        Handles.Label((p1 + p2) / 2, $"{(p1 - p2).magnitude:F3}", new GUIStyle()
+                        Handles.Label((p1 + p2) / 2, $"{(p1 - p2).magnitude * 100.0f:F3}", new GUIStyle()
                         {
                             fontSize = 9
                         });
@@ -75,7 +75,7 @@ public class Estimate : MonoBehaviour
             else
             { //如果没有子级
                 Handles.DrawLine(Vector3.zero, tranRoot.position);
-                Handles.Label(tranRoot.position / 2, $"{tranRoot.position.magnitude:F3}", new GUIStyle()
+                Handles.Label(tranRoot.position / 2, $"{tranRoot.position.magnitude * 100.0f:F3}", new GUIStyle()
                 {
                     fontSize = 9
                 });
